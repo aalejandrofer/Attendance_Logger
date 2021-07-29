8b8d076a9e05b5ce7279a4efcaa65ad86a13813d
AlxGitDocs <alxgitdocs@gmail.com>
2021-07-29 01:06:30 +0100

    update

--- main.py
+++ main.py
@@ -7,27 +7,52 @@ import Modules.serial as serial
 # Install Pip3 to get the requests dependancy
 # Example: sudo apt-get -y install python3-pip python3 && pip3 install requests
 
-# Startin the loop when program starts up
+# False if start entry not entered
+# True if timer is running
+status = False
 
+# Startin the loop when program starts up
 while True:
 
   idRead = serial.read_rfid()
 
   if idRead:
+    #print("Read")
 
-    print("Read")
+    if status == False:
 
-    ids = logger.getIDs()
-    startResponse = logger.startLog()
-    sleep(1)
-    endResponse = logger.terminateLog()
+      ids = logger.getIDs()
+      startResponse = logger.startLog()
 
-    # Logging to txt file
-    with open("RFID.log", "a") as f:
-      now = datetime.now().strftime("%d-%m-%Y %H:%M")
-      f.write(f"{now} : {startResponse}\n")
-      f.write(f"{now} : {endResponse}\n")
-      f.close()
+      status = True
+
+      # Logging to txt file
+      with open("RFID.log", "a") as f:
+        now = datetime.now().strftime("%d-%m-%Y %H:%M")
+        f.write(f"{now} : {startResponse}\n")
+        #f.write(f"{now} : {endResponse}\n")
+        f.close()
+      
+      # Reset Timer
+      sleep(5)
     
+    else:
 
-  
+      ids = logger.getIDs()
+      endResponse = logger.terminateLog()
+
+      # Back to main stage
+      status = False
+
+      # Logging to txt file
+      with open("RFID.log", "a") as f:
+        now = datetime.now().strftime("%d-%m-%Y %H:%M")
+        #f.write(f"{now} : {startResponse}\n")
+        f.write(f"{now} : {endResponse}\n")
+        f.close()
+
+      # Reset Timer
+      sleep(5)
+
+
+
