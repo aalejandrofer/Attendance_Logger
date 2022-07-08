d5528709b4e5fc27f986bfb739d70ee40f2c2488
Buddie <alxgitdocs@gmail.com>
2022-07-08 03:53:37 +0100

    update

--- Modules/timeLimitCheck.py
+++ Modules/timeLimitCheck.py
@@ -1,18 +1,13 @@
 import datetime
 import time
-import os
 from apscheduler.schedulers.background import BackgroundScheduler as Scheduler
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Start the scheduler
 def timeCheckJob():
-    PATH = ROOT_DIR + "/job.conf"
-    with open(PATH, "w") as f:
-        f.write(f"Yep")
+    print("checking time")
 
 def start():
     scheduler = Scheduler()
-    scheduler.add_job(timeCheckJob, 'interval', seconds=5, id="timeLimitJob")
+    scheduler.add_job(timeCheckJob, 'interval', seconds=10, id="timeLimitJob")
     scheduler.start()
     
