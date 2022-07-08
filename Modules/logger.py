3eace95fc47e68f737bf74fff1abcb79afa14563
Buddie <alxgitdocs@gmail.com>
2022-07-08 06:23:01 +0100

    t

--- Modules/logger.py
+++ Modules/logger.py
@@ -125,6 +125,7 @@ def updateEntryOnLimit():
   # https://api.clockify.me/api/v1/workspaces/{{workspaceID}}/time-entries/{{testEntryID}}
   response = requests.put(f"https://api.clockify.me/api/v1/workspaces/{workspace_id}/time-entries/{id}", data=body, headers=headers)
   print(response)
+  print(response.json())
   
   return response
 
